00ac641a95bb049adcfc0fecfa532335167b90e6
Isaev Nikita <nikitavegas95@gmail.com>
2024-12-21 08:02:38 +0300

    feat: without localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import Header from './components/Header';
 import TaskInput from './components/TaskInput';
 import TaskList from './components/TaskList';
 import Filters from './components/Filters';
-import useLocalStorage from "./hooks/useLocalStorage.ts";
+// import useLocalStorage from "./hooks/useLocalStorage.ts";
+import {useState} from "react";
 
 const App = () => {
-    const [tasks, setTasks] = useLocalStorage('tasks', []); // Ключ 'tasks' в localStorage
-    const [filter, setFilter] = useLocalStorage('filter', 'all'); // Храним выбранный фильтр
+    const [tasks, setTasks] = useState([]);
+    const [filter, setFilter] = useState('all');
+
+    // const [tasks, setTasks] = useLocalStorage('tasks', []); // Ключ 'tasks' в localStorage
+    // const [filter, setFilter] = useLocalStorage('filter', 'all'); // Храним выбранный фильтр
 
 
     return (
